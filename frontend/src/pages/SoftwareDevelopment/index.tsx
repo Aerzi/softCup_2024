2c3cdf7161ace8df974d457a0eb468e5b584094c
s_xujiacheng <3158928594@qq.com>
2024-07-14 23:43:43 +0800

    feat: 添加编程开发页面

---
+++ frontend/src/pages/SoftwareDevelopment/index.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import NavHeader from "../../components/NavHeader";
+import "./index.less";
+import CodeSphere from "../../components/CodeSphere";
+import { ProCard } from "@ant-design/pro-components";
+
+const SoftwareDevelopment = () => {
+  const [code, setCode] = useState('');
+
+  const handleEditorChange = (newValue: string) => {
+    setCode(newValue);
+  };
+
+  return (
+    <>
+      <div className="xf-soft__page">
+        <header className="xf-soft__header">
+          <NavHeader />
+        </header>
+        <main className="xf-soft__main">
+          <div className="xf-soft__main-content">
+            <div className="xf-soft__main-content-left">
+              <ProCard  bordered boxShadow className="xf-soft__main--content-left-card"></ProCard>
+            </div>
+            <div className="xf-soft__main-content-right">
+              <CodeSphere 
+              value={code}
+              language="typescript"
+              onChange={handleEditorChange} 
+              />
+            </div>
+          </div>
+        </main>
+        <footer className="xf-soft__footer"></footer>
+      </div>
+      <div className="xf-soft__mask"></div>
+    </>
+  );
+};
+
+export default SoftwareDevelopment;
