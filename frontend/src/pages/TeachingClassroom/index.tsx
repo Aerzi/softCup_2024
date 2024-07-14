8a3354a293c8ec18908149f81325e0919d4ab4a6
s_xujiacheng <3158928594@qq.com>
2024-07-14 23:44:54 +0800

    feat: 编辑教学课堂布局，添加布局样式

--- frontend/src/pages/TeachingClassroom/index.tsx
+++ frontend/src/pages/TeachingClassroom/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./index.less";
 import NavHeader from "../../components/NavHeader";
+import { ProCard } from "@ant-design/pro-components";
 
 const TeachingClassroom = () => {
   return (
@@ -9,7 +10,23 @@ const TeachingClassroom = () => {
         <header className="xf-teaching__header">
           <NavHeader />
         </header>
-        <main className="xf-teaching__main"></main>
+        <main className="xf-teaching__main">
+          <div className="xf-teaching__main-content">
+            <ProCard style={{ marginBlockStart: 8 }} gutter={8} ghost>
+              <ProCard colSpan="300px" layout="center" direction="column" bordered className="xf-teaching__main-content-card-left">
+                <ProCard colSpan="300px" layout="center" bordered className="xf-teaching__main-content-card-left--1">
+                  colSpan - 300px
+                </ProCard>
+                <ProCard colSpan="300px" layout="center" bordered className="xf-teaching__main-content-card-left--2">
+                  colSpan - 300px
+                </ProCard>
+              </ProCard>
+              <ProCard layout="center" bordered className="xf-teaching__main-content-card-right">
+                Auto
+              </ProCard>
+            </ProCard>
+          </div>
+        </main>
         <footer className="xf-teaching__footer"></footer>
       </div>
       <div className="xf-teaching__mask"></div>
