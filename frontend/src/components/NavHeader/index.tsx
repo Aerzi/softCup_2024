dc0f13d66423768ff49bf95c2580cfd1e7cb460b
s_xujiacheng <3158928594@qq.com>
2024-07-16 00:46:06 +0800

    fix: 为navHeader添加状态管理

--- frontend/src/components/NavHeader/index.tsx
+++ frontend/src/components/NavHeader/index.tsx
@@ -2,10 +2,23 @@ import React, { useState } from "react";
 import "./index.less";
 import { NavLink } from "react-router-dom";
 import UserModal from "../UserModal";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../stores/redux/store";
+import { updateUser } from "../../stores/slices/userSlice";
 
 const NavHeader = () => {
   const [isUserOpen, setIsUserOpen] = useState(false);
   // 对于导航栏，需要设置两种状态，一种是已登录，一种是未登录
+  // 引入user, 有student和teacher两种状态
+  const user = useSelector((state: RootState) => state.user);
+
+  // 使用useDispatch钩子获取dispatch函数
+  const dispatch = useDispatch();
+  // 定义一个切换角色的函数
+  const toggleRole = () => {
+    // 根据当前角色切换到另一个角色
+    dispatch(updateUser(!(user.role === "student")));
+  };
 
   return (
     <nav className="xf-header__nav">
@@ -20,36 +33,75 @@ const NavHeader = () => {
             系统首页
           </NavLink>
         </li>
-        <li className="xf-header__nav-item">
-          <NavLink
-            to={"/studyplan"}
-            className={({ isActive }) =>
-              `link ${isActive ? "xf-header__nav-item-link--active" : "xf-header__nav-item-link"}`
-            }
-          >
-            学习规划
-          </NavLink>
-        </li>
-        <li className="xf-header__nav-item">
-          <NavLink
-            to={"/teachingroom"}
-            className={({ isActive }) =>
-              `link ${isActive ? "xf-header__nav-item-link--active" : "xf-header__nav-item-link"}`
-            }
-          >
-            教学课堂
-          </NavLink>
-        </li>
-        <li className="xf-header__nav-item">
-          <NavLink
-            to={"/softwaredevelopment"}
-            className={({ isActive }) =>
-              `link ${isActive ? "xf-header__nav-item-link--active" : "xf-header__nav-item-link"}`
-            }
-          >
-            编程开发
-          </NavLink>
-        </li>
+
+        {user.role === "student" ? (
+          <>
+            <li className="xf-header__nav-item">
+              <NavLink
+                to={"/studyplan"}
+                className={({ isActive }) =>
+                  `link ${isActive ? "xf-header__nav-item-link--active" : "xf-header__nav-item-link"}`
+                }
+              >
+                学习规划
+              </NavLink>
+            </li>
+            <li className="xf-header__nav-item">
+              <NavLink
+                to={"/teachingroom"}
+                className={({ isActive }) =>
+                  `link ${isActive ? "xf-header__nav-item-link--active" : "xf-header__nav-item-link"}`
+                }
+              >
+                教学课堂
+              </NavLink>
+            </li>
+            <li className="xf-header__nav-item">
+              <NavLink
+                to={"/softwaredevelopment"}
+                className={({ isActive }) =>
+                  `link ${isActive ? "xf-header__nav-item-link--active" : "xf-header__nav-item-link"}`
+                }
+              >
+                编程开发
+              </NavLink>
+            </li>
+          </>
+        ) : (
+          <>
+            <li className="xf-header__nav-item">
+              <NavLink
+                to={"/schedulemanagement"}
+                className={({ isActive }) =>
+                  `link ${isActive ? "xf-header__nav-item-link--active" : "xf-header__nav-item-link"}`
+                }
+              >
+                日程管理
+              </NavLink>
+            </li>
+            <li className="xf-header__nav-item">
+              <NavLink
+                to={"/classmanagement"}
+                className={({ isActive }) =>
+                  `link ${isActive ? "xf-header__nav-item-link--active" : "xf-header__nav-item-link"}`
+                }
+              >
+                班级管理
+              </NavLink>
+            </li>
+            <li className="xf-header__nav-item">
+              <NavLink
+                to={"/homeworkmanagement"}
+                className={({ isActive }) =>
+                  `link ${isActive ? "xf-header__nav-item-link--active" : "xf-header__nav-item-link"}`
+                }
+              >
+                作业管理
+              </NavLink>
+            </li>
+          </>
+        )}
+
         <li className="xf-header__nav-item">
           <NavLink
             to={"/userprofile"}
