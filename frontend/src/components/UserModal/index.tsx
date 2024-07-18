d3505fb908b42daedcebd29c258ec1f8701d94cd
s_xujiacheng <3158928594@qq.com>
2024-07-18 22:56:31 +0800

    feat: 用户登录服务修改，添加登录注册

--- frontend/src/components/UserModal/index.tsx
+++ frontend/src/components/UserModal/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./index.less";
-import { Button, Form, Input, Modal, Tabs } from "antd";
+import { Button, Form, Input, message, Modal, Tabs } from "antd";
 import type { TabsProps } from "antd";
 import { ProCard } from "@ant-design/pro-components";
 import student from "./student.svg";
@@ -9,6 +9,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../stores/redux/store";
 import { updateUser } from "../../stores/slices/userSlice";
 import { use } from "echarts";
+import {
+  onLogin,
+  onLogout,
+  onStudentRegister,
+  onTeacherRegister,
+} from "../../services/userService";
+import { setLocalData } from "../../utils/Storage";
 
 interface UserModalData {
   isOpen: boolean;
@@ -22,9 +29,14 @@ type FieldType = {
 };
 
 const UserModal = ({ isOpen, setIsOpen }: UserModalData) => {
-  const [login] = Form.useForm();
+  // 全局挂载messageApi
+  const [messageApi, contextHolder] = message.useMessage();
 
+  const [login] = Form.useForm();
   const [register] = Form.useForm();
+
+  // tabs激活页管理
+  const [activeTabsKey, setActiveTabsKey] = useState("1");
 
   // 引入user, 有student和teacher两种状态
   const user = useSelector((state: RootState) => state.user);
@@ -39,9 +51,64 @@ const UserModal = ({ isOpen, setIsOpen }: UserModalData) => {
     );
   };
 
-  // 定义一个切换是否登录的函数
-  const toggleLogin = () => {
-    dispatch(updateUser({ isLogin: !user.isLogin }));
+  const onUserLogin = (username: string, password: string) => {
+    // 调用统一登录接口，不需要对角色做出判断
+    onLogin(username, password)
+      .then((res: any) => {
+        console.log("res为", res);
+        if (res.code === 200) {
+          console.log("用户登录成功");
+          // 说明成功，调用messageApi表示目前登录状态ok
+          messageApi.open({ type: "success", content: res.message });
+          // 保存用户信息
+          setLocalData("user", res.response);
+          dispatch(updateUser({ isLogin: true, data: res.response }));
+        } else {
+          messageApi.open({ type: "error", content: res.message });
+        }
+      })
+      .catch((err: any) => {
+        console.error(err.message);
+        messageApi.open({ type: "error", content: err.message });
+      });
+  };
+
+  const onUserRegister = (username: string, password: string) => {
+    // 调用注册接口，需要对角色做出判断
+    if (user.role === "student") {
+      onStudentRegister(username, password)
+        .then((res: any) => {
+          console.log("res.code为", res.code);
+          if (res.code === 200) {
+            console.log("成功了");
+            // 说明成功，调用messageApi表示目前注册状态ok
+            messageApi.open({ type: "success", content: res.message });
+            // 回到登录tabs
+            setActiveTabsKey("1");
+          } else {
+            messageApi.open({ type: "error", content: res.message });
+          }
+        })
+        .catch((err: any) => {
+          console.log("被拦截了");
+          messageApi.open({ type: "error", content: err.message });
+        });
+    } else {
+      onTeacherRegister(username, password)
+        .then((res: any) => {
+          if (res.code === 200) {
+            // 说明成功，调用messageApi表示目前登录状态ok
+            messageApi.open({ type: "success", content: res.message });
+            // 回到登录tabs
+            setActiveTabsKey("1");
+          } else {
+            messageApi.open({ type: "error", content: res.message });
+          }
+        })
+        .catch((err: any) => {
+          messageApi.open({ type: "error", content: err.message });
+        });
+    }
   };
 
   const items: TabsProps["items"] = [
@@ -54,9 +121,7 @@ const UserModal = ({ isOpen, setIsOpen }: UserModalData) => {
             <Form.Item<FieldType>
               label="用户名"
               name="username"
-              rules={[
-                { required: true, message: "Please input your username!" },
-              ]}
+              rules={[{ required: true, message: "用户名不能为空!" }]}
             >
               <Input />
             </Form.Item>
@@ -65,7 +130,16 @@ const UserModal = ({ isOpen, setIsOpen }: UserModalData) => {
               label="密码"
               name="password"
               rules={[
-                { required: true, message: "Please input your password!" },
+                { required: true, message: "密码不能为空!" },
+                { min: 4, message: "密码长度不能少于4位!" },
+                {
+                  validator: (rule, value) => {
+                    if (value && !/[0-9]/.test(value)) {
+                      return Promise.reject(new Error("密码必须包含数字!"));
+                    }
+                    return Promise.resolve();
+                  },
+                },
               ]}
             >
               <Input.Password />
@@ -75,7 +149,12 @@ const UserModal = ({ isOpen, setIsOpen }: UserModalData) => {
               <Button
                 type="primary"
                 htmlType="submit"
-                onClick={() => toggleLogin()}
+                onClick={() =>
+                  onUserLogin(
+                    login.getFieldValue("username"),
+                    login.getFieldValue("password")
+                  )
+                }
               >
                 登录
               </Button>
@@ -93,9 +172,7 @@ const UserModal = ({ isOpen, setIsOpen }: UserModalData) => {
             <Form.Item<FieldType>
               label="用户名"
               name="username"
-              rules={[
-                { required: true, message: "Please input your username!" },
-              ]}
+              rules={[{ required: true, message: "用户名不能为空!" }]}
             >
               <Input />
             </Form.Item>
@@ -104,7 +181,19 @@ const UserModal = ({ isOpen, setIsOpen }: UserModalData) => {
               label="密码"
               name="password"
               rules={[
-                { required: true, message: "Please input your password!" },
+                { required: true, message: "密码不能为空!" },
+                { min: 6, message: "密码长度不能少于6位!" },
+                {
+                  validator: (rule, value) => {
+                    if (value && !/[0-9]/.test(value)) {
+                      return Promise.reject(new Error("密码必须包含数字!"));
+                    }
+                    if (value && !/[a-zA-Z]/.test(value)) {
+                      return Promise.reject(new Error("密码必须包含字母!"));
+                    }
+                    return Promise.resolve();
+                  },
+                },
               ]}
             >
               <Input.Password />
@@ -112,11 +201,18 @@ const UserModal = ({ isOpen, setIsOpen }: UserModalData) => {
 
             <Form.Item<FieldType>
               label="确认密码"
-              name="password"
+              name="remember"
               rules={[
+                { required: true, message: "请输入确认密码!" },
                 {
-                  required: true,
-                  message: "Please input your password again!",
+                  validator: (_) => {
+                    const password = register.getFieldValue("password");
+                    const confirm = register.getFieldValue("remember");
+                    if (password && confirm && password !== confirm) {
+                      return Promise.reject(new Error("确认密码与密码不一致!"));
+                    }
+                    return Promise.resolve();
+                  },
                 },
               ]}
             >
@@ -124,7 +220,27 @@ const UserModal = ({ isOpen, setIsOpen }: UserModalData) => {
             </Form.Item>
 
             <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-              <Button type="primary" htmlType="submit">
+              <Button
+                type="primary"
+                htmlType="submit"
+                onClick={() => {
+                  if (
+                    register.getFieldValue("password") !==
+                      register.getFieldValue("remember") ||
+                    register.getFieldValue("password") === null
+                  ) {
+                    messageApi.open({
+                      type: "error",
+                      content: "确认密码与密码不一致!",
+                    });
+                    return;
+                  }
+                  onUserRegister(
+                    register.getFieldValue("username"),
+                    register.getFieldValue("password")
+                  );
+                }}
+              >
                 注册
               </Button>
             </Form.Item>
@@ -143,51 +259,58 @@ const UserModal = ({ isOpen, setIsOpen }: UserModalData) => {
   };
 
   const onTabsChange = (key: string) => {
-    console.log(key);
+    setActiveTabsKey(key);
   };
 
   return (
-    <div>
-      <Modal
-        title="登录/注册"
-        width={850}
-        open={isOpen}
-        onOk={handleOk}
-        onCancel={handleCancel}
-        footer={null}
-        className="xf-user__modal"
-      >
-        <div className="xf-user__modal-content">
-          <span className="xf-user__modal-tabs">
-            <Tabs defaultActiveKey="1" items={items} onChange={onTabsChange} />
-          </span>
-          {/* 除了进行登录注册的筛选外，还需要进行老师和学生的筛选 */}
-          <span className="xf-user__modal-card">
-            <ProCard
-              bordered
-              style={{ width: "100%", height: "100%" }}
-              className="xf-user__modal-card-body"
-              onClick={() => toggleRole()}
-            >
-              {/* 判断之后，展示导入的图片 */}
-              {user.role === "student" ? (
-                <div className="xf-user__modal-card-body-content">
-                  <img src={student} className="xf-user__modal-card-img" />
-                  <span className="xf-user__modal-card-text">我是学生</span>
-                </div>
-
-              ) : (
-                <div className="xf-user__modal-card-body-content">
-                  <img src={teacher} className="xf-user__modal-card-img" />
-                  <span className="xf-user__modal-card-text">我是老师</span>
-                </div>
-
-              )}
-            </ProCard>
-          </span>
-        </div>
-      </Modal>
-    </div>
+    <>
+      {contextHolder}
+      <div>
+        <Modal
+          title="登录/注册"
+          width={850}
+          open={isOpen}
+          onOk={handleOk}
+          onCancel={handleCancel}
+          footer={null}
+          className="xf-user__modal"
+        >
+          <div className="xf-user__modal-content">
+            <span className="xf-user__modal-tabs">
+              <Tabs
+                defaultActiveKey="1"
+                items={items}
+                onChange={onTabsChange}
+                activeKey={activeTabsKey}
+                animated
+              />
+            </span>
+            {/* 除了进行登录注册的筛选外，还需要进行老师和学生的筛选 */}
+            <span className="xf-user__modal-card">
+              <ProCard
+                bordered
+                style={{ width: "100%", height: "100%" }}
+                className="xf-user__modal-card-body"
+                onClick={() => toggleRole()}
+              >
+                {/* 判断之后，展示导入的图片 */}
+                {user.role === "student" ? (
+                  <div className="xf-user__modal-card-body-content">
+                    <img src={student} className="xf-user__modal-card-img" />
+                    <span className="xf-user__modal-card-text">我是学生</span>
+                  </div>
+                ) : (
+                  <div className="xf-user__modal-card-body-content">
+                    <img src={teacher} className="xf-user__modal-card-img" />
+                    <span className="xf-user__modal-card-text">我是老师</span>
+                  </div>
+                )}
+              </ProCard>
+            </span>
+          </div>
+        </Modal>
+      </div>
+    </>
   );
 };
 
