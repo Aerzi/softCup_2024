1d6a1d536f2aea5d9e985f69744ed8bb13a23fd5
s_xujiacheng <3158928594@qq.com>
2024-07-18 22:58:17 +0800

    feat: 添加用户服务

--- frontend/src/services/userService/index.ts
+++ frontend/src/services/userService/index.ts
@@ -1,71 +1,78 @@
+import { Token } from "monaco-editor";
 import request from "../http/http";
+import { getLocalData } from "../../utils/Storage";
 
 // 统一接口
 
 /**
  * 统一登录接口 -- 登录后开放用户权限，允许访问所有网页
- * @param userName 
- * @param password 
- * @returns 
+ * @param userName
+ * @param password
+ * @returns
  */
 export const onLogin = (userName: string, password: string) => {
-    return request({
-        url: 'user/login',
-        method: 'post',
-        data: {
-            userName: userName,
-            password: password
-        },
-        headers:{}
-    })
-}
+  return request(
+    "post",
+    "user/login",
+    {
+      userName: userName,
+      password: password,
+    },
+    {}
+  );
+};
 
 /**
  * 统一注销接口 -- 注销后退出系统
- * @returns 
+ * @returns
  */
 export const onLogout = () => {
-    return request({
-        url: 'user/logout',
-        method: 'get',
-        headers:{}
-    })
-}
+  return request(
+    "get",
+    "user/logout",
+    {},
+    {
+      headers: {
+        token: getLocalData("token") !== null ? getLocalData("token") : "",
+      },
+    }
+  );
+};
 
 // 学生相关
 /**
  * 学生注册接口
- * @param userName 
- * @param password 
- * @returns 
+ * @param userName
+ * @param password
+ * @returns
  */
 export const onStudentRegister = (userName: string, password: string) => {
-    return request({
-        url: 'student/user/register',
-        method: 'post',
-        data: {
-            userName: userName,
-            password: password
-        },
-        headers:{}
-    })
-}
+  return request(
+    "post",
+    "student/user/register",
+    {
+      userName: userName,
+      password: password,
+    },
+    {}
+  );
+};
 
 // 教师相关
 /**
  * 教师注册接口
- * @param userName 
- * @param password 
- * @returns 
+ * @param userName
+ * @param password
+ * @returns
  */
 export const onTeacherRegister = (userName: string, password: string) => {
-    return request({
-        url: 'teacher/user/register',
-        method: 'post',
-        data: {
-            userName: userName,
-            password: password
-        },
-        headers:{}
-    })
-}
+  return request(
+    "post",
+    "teacher/user/register",
+    {
+      userName: userName,
+      password: password,
+    },
+    {}
+  );
+};
