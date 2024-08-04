9174022e205b7f330011cb45effc1a5597ef1dab
feixia0g <591337215@qq.com>
2024-08-05 01:26:37 +0800

    fix:添加学生端文档问答的文件url * 添加学生端文档问答的文件url

---
+++ backend/src/main/java/com/example/backend/model/request/student/spark/chatdoc/ChatDocApiUrlResponse.java
@@ -0,0 +1,11 @@
+package com.example.backend.model.request.student.spark.chatdoc;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatDocApiUrlResponse {
+    ChatDocApiResponse response;
+    String url;
+}
