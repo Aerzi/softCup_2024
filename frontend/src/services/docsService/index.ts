f5d46077064c885445901c3018044ec30b8a6366
s_xujiacheng <3158928594@qq.com>
2024-08-06 00:25:25 +0800

    feat: 添加文档请求接口

--- frontend/src/services/docsService/index.ts
+++ frontend/src/services/docsService/index.ts
@@ -1,4 +1,5 @@
-import { post } from "../config/config";
+import { IDocs } from "../../components/Docs/type";
+import { get, post } from "../config/config";
 
 // 首先通过upload上传文件后建立连接
 export const uploadDocs = (formData: FormData) => {
@@ -9,11 +10,38 @@ export const chatDocs = ({
   fileId,
   question,
 }: {
-  fileId: number;
+  fileId: string;
   question: string;
 }) => {
   return post("student/spark/chat/doc/chat", {
     fileId,
     question,
   });
+};
+
+export const rewriteDocs = ({
+  level,
+  text,
+}: {
+  level: number;
+  text: string;
+}) => {
+  return post("student/spark/rewrite/generate", {
+    level,
+    text,
+  });
+};
+
+export const pptByDocs = (formData: FormData) => {
+  return post("student/spark/ppt/doc/ppt", formData);
+};
+
+// 获取主题列表
+export const getThemeList = () => {
+  return get("student/spark/ppt/themeList");
+};
+
+// 获取进度条
+export const getProcess = (sid: string) => {
+  return get(`/student/spark/ppt/ppt/progress/${sid}`);
 };
